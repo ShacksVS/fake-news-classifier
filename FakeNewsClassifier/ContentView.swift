10c0223b44b14dd711a70ac92b7dc44e3c3c0d35
ShacksVS <inode16@gmail.com>
2024-06-14 16:07:11 +0300

    basic UI implementation

--- FakeNewsClassifier/ContentView.swift
+++ FakeNewsClassifier/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputText = ""
+    @State private var showAlert = false
+    @State private var result = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextEditor(
+                text: $inputText
+            )
+            .frame(width: 332, height: 100)
+            .padding(EdgeInsets(top: 12, leading: 15, bottom: 12, trailing: 15))
+            .cornerRadius(50)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 1)
+            )
+            
+            Button {
+                // Change it with actual model response
+                result = "Fake or Not Fake"
+                showAlert = true
+            } label: {
+                Text("Detect")
+                    .font(.system(size: 16))
+            }
+            .frame(width: 100, height: 50)
+            .foregroundColor(.primary)
+            .background(Color(UIColor.systemYellow))
+            .cornerRadius(10)
+            .padding(.top)
+            .alert(isPresented: $showAlert) {
+                Alert(
+                    title: Text("Response"),
+                    // Add probability of each classes??
+                    message: Text("The result of classification: \(result)"),
+                    dismissButton: .default(Text("OK")))
+            }
         }
         .padding()
     }
