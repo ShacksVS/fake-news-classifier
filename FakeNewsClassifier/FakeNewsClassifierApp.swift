db41e56b2420acffcc082b72de295aefb141bf2c
ShacksVS <inode16@gmail.com>
2024-06-14 15:47:03 +0300

    initial, added Beta Fake Classification Model

---
+++ FakeNewsClassifier/FakeNewsClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeNewsClassifierApp.swift
+//  FakeNewsClassifier
+//
+//  Created by Viktor Sovyak on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FakeNewsClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
